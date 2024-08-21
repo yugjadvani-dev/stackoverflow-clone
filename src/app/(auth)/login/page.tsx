032d9a5b20985a9896e249a57075039efd9ce3ad
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-08-21 23:33:17 +0530

    add BackgroundBeamsWithCollision

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -40,6 +41,7 @@ export default function LoginPage() {
   }, [user]);
 
   return (
+    <BackgroundBeamsWithCollision>
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
@@ -67,5 +69,6 @@ export default function LoginPage() {
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
       <Link href="/signup">Visit Signup page</Link>
     </div>
+    </BackgroundBeamsWithCollision>
   );
 }
